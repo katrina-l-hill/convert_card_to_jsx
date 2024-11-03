331972de283929fa1ddf365f67ad10434a08eadf
Katrina L Hill <khill365@gmail.com>
2024-11-03 15:37:41 -0800

    Initial commit

---
+++ html-to-jsx/src/app/components/UserProfile.js
@@ -0,0 +1,18 @@
+// components/UserProfile.js
+import React from 'react';
+  
+function UserProfile(props) {
+  return (
+
+    <div className='profile-card'>
+    <h2>User Profile</h2>
+    <img src={props.photoUrl} alt="User Photo" class="photo"/>
+    <p>Name: {props.name}</p>
+    <p>Email: {props.email}</p>
+    <a href={`mailto:${props.email}`}>Send Email</a>
+  </div> 
+
+  );
+}
+
+export default UserProfile;
